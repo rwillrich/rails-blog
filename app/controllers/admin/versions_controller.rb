d3784cee1067d3294ce6ad5a86ae3a6f8609ab6c
Rodrigo Willrich <rodrigo.willrich@hotmail.com>
2014-12-20 15:22:10 -0200

    Change Undo button to always show 'Undid'

--- app/controllers/admin/versions_controller.rb
+++ app/controllers/admin/versions_controller.rb
@@ -13,8 +13,7 @@ module Admin
       button_name = is_redo ? 'Undo' : 'Redo'
       revert_path = revert_admin_version_path(@version.next, redo: !is_redo)
       link = view_context.button_to(button_name, revert_path, method: :post)
-      action = is_redo ? 'Redid' : 'Undid'
-      redirect_to :back, notice: "#{action} #{@version.event}. #{link}"
+      redirect_to :back, notice: "Undid #{@version.event}. #{link}"
     end
 
   end
